ca39e73d1232cf6c8af1866d2c3af325c018e23a
djsmedes <djsmedes@gmail.com>
2018-03-25 23:29:01 -0500

    basic template updates

--- dmtools/urls.py
+++ dmtools/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html')),
     path('places/', include('places.urls')),
     path('people/', include('people.urls')),
     path('plot/', include('plot.urls')),
