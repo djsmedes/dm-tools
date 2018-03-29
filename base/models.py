566086a4346e2d115cdeff75bf6875ae62c17723
djsmedes <djsmedes@gmail.com>
2018-03-28 23:48:46 -0500

    moving BaseModel from people to base

--- base/models.py
+++ base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
+class BaseModel(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def meta(self):
+        return self._meta
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse('{}-view'.format(self._meta.verbose_name), kwargs={'pk': self.pk})
+
+    def get_create_url(self):
+        return reverse('{}-add'.format(self._meta.verbose_name))
+
+    def get_delete_url(self):
+        return reverse('{}-delete'.format(self._meta.verbose_name), kwargs={'pk': self.pk})
--- people/models.py
+++ people/models.py
@@ -1,30 +1,7 @@
 from django.db import models
 from django.forms import ModelForm, ModelMultipleChoiceField
-from django.shortcuts import reverse
 
-
-class BaseModel(models.Model):
-
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-    def meta(self):
-        return self._meta
-
-    class Meta:
-        abstract = True
-        ordering = ['name']
-
-    def get_absolute_url(self):
-        return reverse('{}-view'.format(self._meta.verbose_name), kwargs={'pk': self.pk})
-
-    def get_create_url(self):
-        return reverse('{}-add'.format(self._meta.verbose_name))
-
-    def get_delete_url(self):
-        return reverse('{}-delete'.format(self._meta.verbose_name), kwargs={'pk': self.pk})
+from base.models import BaseModel
 
 
 class Person(BaseModel):
