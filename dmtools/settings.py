cd5882936763dd76abebdf297ebfed3a26eb6338
djsmedes <djsmedes@gmail.com>
2018-03-28 23:38:29 -0500

    moving project level templates to base app

--- dmtools/settings.py
+++ dmtools/settings.py
@@ -69,9 +69,7 @@ ROOT_URLCONF = 'dmtools.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
