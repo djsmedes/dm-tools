9b759e96d68c015726096b3088e30ecc550a8c19
djsmedes <djsmedes@gmail.com>
2018-04-09 09:54:24 -0500

    fixing up django-autocomplete-light with a hack to make it work properly

--- dmtools/settings/base.py
+++ dmtools/settings/base.py
@@ -140,7 +140,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/dist/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
