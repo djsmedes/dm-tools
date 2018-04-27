71c962af1e5b1b861cce9e4359b073b0207e8e3d
djsmedes <djsmedes@gmail.com>
2018-04-27 16:33:35 -0500

    created urls structure, view, template

--- places/urls.py
+++ places/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 
+from dmtools.urls import breadcrumbs as core_breadcrumbs
+from .views import CanvasView
+
+breadcrumbs = core_breadcrumbs
+
+places_breadcrumbs = breadcrumbs + [{'href': reverse_lazy('places-home'), 'text': 'Places'}]
+
+places_patterns = [
+    path('', CanvasView.as_view(), name='places-home')
+]
 
 urlpatterns = [
-    
+    path('', include(places_patterns), {'base_breadcrumbs': places_breadcrumbs})
 ]
