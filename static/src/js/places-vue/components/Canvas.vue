1b50cfd3ca016fd33f40c8e706822e46ec1f2c29
djsmedes <djsmedes@gmail.com>
2018-04-29 23:03:01 -0500

    formatting

--- static/src/js/places-vue/components/Canvas.vue
+++ static/src/js/places-vue/components/Canvas.vue
@@ -12,14 +12,10 @@
       </defs>
 
       <g v-for="shape in shapes">
-        <polygon :points="shape.points" stroke="green" fill="green" stroke-width="2" filter="url(#innershadow)"></polygon>
+        <polygon :points="shape.points" stroke="green" fill="green" stroke-width="2"
+                 filter="url(#innershadow)"></polygon>
         <polygon :points="shape.points" fill="transparent" stroke="green" stroke-width="2"></polygon>
       </g>
-
-      <!--<polygon points="200,50 300,50 250,190 160,210" filter="url(#innershadow)" fill="green" stroke="green"-->
-               <!--stroke-width="2"></polygon>-->
-
-      <!--<polygon points="200,50 300,50 250,190 160,210" fill="transparent" stroke="green" stroke-width="2"></polygon>-->
 
       <rect width="1200" height="900" fill="transparent" stroke="black" stroke-width="2"
             v-on:click="test_method($event)">
@@ -31,34 +27,40 @@
 </template>
 
 <script>
-  import axios from 'axios';
+    import axios from 'axios';
 
-  axios.defaults.xsrfCookieName = 'csrftoken';
-  axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+    axios.defaults.xsrfCookieName = 'csrftoken';
+    axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-  export default {
-      data() {
-          return {
-              shapes: []
-          }
-      },
-      methods: {
-          load_shapes: function() {
-              axios
-                  .get('/places/api/get-place-data/')
-                  .then(r => { this.shapes = r.data.shape_set })
-                  .catch(e => { console.log(e) })
-          },
-          test_method: function(event) {
-              let bound = document.getElementById('place-canvas').getBoundingClientRect();
-              let html = document.documentElement;
-              let left = bound.left + window.pageXOffset - html.clientLeft;
-              let top = bound.top + window.pageYOffset - html.clientTop;
-              let x = event.pageX - left;
-              let y = event.pageY - top;
-              console.log(x, y);
-          }
-      },
-      created() { this.load_shapes() }
-  }
+    export default {
+        data() {
+            return {
+                shapes: []
+            }
+        },
+        methods: {
+            load_shapes: function () {
+                axios
+                    .get('/places/api/get-place-data/')
+                    .then(r => {
+                        this.shapes = r.data.shape_set
+                    })
+                    .catch(e => {
+                        console.log(e)
+                    })
+            },
+            test_method: function (event) {
+                let bound = document.getElementById('place-canvas').getBoundingClientRect();
+                let html = document.documentElement;
+                let left = bound.left + window.pageXOffset - html.clientLeft;
+                let top = bound.top + window.pageYOffset - html.clientTop;
+                let x = event.pageX - left;
+                let y = event.pageY - top;
+                console.log(x, y);
+            }
+        },
+        created() {
+            this.load_shapes()
+        }
+    }
 </script>
