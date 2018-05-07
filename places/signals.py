153d4b7ea7341d9703d704466e8a5921517e1a91
djsmedes <djsmedes@gmail.com>
2018-05-06 19:55:26 -0500

    tweak to signal for code style

--- places/signals.py
+++ places/signals.py
@@ -5,15 +5,15 @@ from places.models import Place, PlacePair
 
 def create_place_pairs(sender, instance: Place, **kwargs):
     # todo: change this queryset to filter so that only places that SHOULD have a relationship get one
-    other_places = Place.objects.all()
+    other_instances = Place.objects.all()
 
-    for other_place in other_places:
-        if instance is other_place:
+    for other_instance in other_instances:
+        if instance is other_instance:
             continue
-        min_distance = instance.shape.distance(other_place.shape)
+        min_distance = instance.shape.distance(other_instance.shape)
         PlacePair.objects.create(
             place1=instance,
-            place2=other_place,
+            place2=other_instance,
             min_distance=min_distance
         )
 
